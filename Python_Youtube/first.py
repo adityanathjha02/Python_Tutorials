e1247a1d2f5fb1e024460d98ae96e8fee40f71de
adityanathjha02 <adityanathjha.hp@hotmail.com>
2025-06-07 21:15:06 +0530

    Add Python and Python_Youtube folders with contents

---
+++ Python_Youtube/first.py
@@ -0,0 +1,19 @@
+print("hello world")
+<<<<<<< HEAD
+number=int(input("enter a number:"))
+if(number%10==0):
+    print("divisible of 10")
+
+
+elif(number%10==1):
+    print("dfdsfdsf")
+
+
+else:
+    print("svfsbvdfsabvdfabvdfbdfbdfbdfb")
+=======
+a =10
+b = 15
+c = a+b
+print(c)
+>>>>>>> desktop
---
+++ Python_Youtube/third.py
@@ -0,0 +1,25 @@
+info={
+    "key":"value",
+    "name":["python","C","Java"],
+    "subject":("dict","set"),
+    "age":35,
+    "isAdult":True,
+    2 :"fdfdsfd",
+    3:35,
+    
+}
+info["name"][0]="c++"
+print(info.keys())
+print(info["name"])
+
+
+students={
+    "name":"Aditya Nath Jha",
+    "subject":{
+        "phy":95.8,
+        "chem":93,
+        "maths":91
+    }
+}
+
+print(students.keys())
